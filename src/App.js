509d362fc2f5c8c92e824ede03afb3300d821457
renyellen <reny1934@gmail.com>
2022-10-20 02:26:55 -0300

    evoluindo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import * as S from "./Style/GlobalStyle.js";
 import ODS from "./Components/Ods.js";
 import Home from "./Components/Home.js";
 import Certificados from "./Components/Certificados.js";
-import Works from"./Components/Works.js";
+import Work from"./Components/Works.js";
+<div>
 <Router>
       <S.GlobalStyle />
       <S.Header>
@@ -20,14 +21,17 @@ import Works from"./Components/Works.js";
               <S.Li>ODS</S.Li>
             </S.Link>
           </S.Ul>
-        </S.Nav>
-      </S.Header>
-      <Routes>
-        <Route path="/ODS element={<ODS />} />
+          <S.Link href="/Certificados">
+              <S.Li>Certificados</S.Li>
+            </S.Link>
+</S.Nav>
+</S.Header>
+             <Routes>
+        <Route path="/ODS" element={<ODS />} />
         <Route path="/" element={<Home />} />
-        <Route path="/Work" element={<Work />} />]
-      </Routes>
+        <Route path="/Work" element={<Work />} />
+        <Route path="=/Certificados" element={<Certificados/>}
+      
+  
     </Router>
-  );
-}
-
+    </div>
